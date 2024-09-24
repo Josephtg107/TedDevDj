381b0269c6bc65cba80ad00a7ffa0619a07da44b
Joseph <josephtg107@gmail.com>
2024-09-24 07:34:37 -0500

    Initial Commit

---
+++ TedDevDj/TedDevDjApp.swift
@@ -0,0 +1,17 @@
+//
+//  TedDevDjApp.swift
+//  TedDevDj
+//
+//  Created by Joseph Garcia on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TedDevDjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TedDevDjTests/TedDevDjTests.swift
@@ -0,0 +1,17 @@
+//
+//  TedDevDjTests.swift
+//  TedDevDjTests
+//
+//  Created by Joseph Garcia on 24/09/24.
+//
+
+import Testing
+@testable import TedDevDj
+
+struct TedDevDjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
